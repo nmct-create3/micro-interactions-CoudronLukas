d745e8b38eaebbee7a5b0ff2e42297b1ada50c22
CoudronLukas <lukas.coudron@student.howest.be>
2020-10-08 16:38:48 +0200

    Password button

--- script/app.js
+++ script/app.js
@@ -3,15 +3,22 @@ function handleFloatingLabel() {
 }
 
 function handlePasswordSwitcher() {
-    var password = document.getElementById('ShowPassword');
-    if(password.type  === "password"){
-        password.type = "text";
-    }
-    else{
-        password.type = "password";
-    }
-}
 
+    let checkbox = document.querySelector('.c-toggle-password__checkbox');
+    let passwordInput = document.querySelector('.c-toggle-password__input');
+    
+    checkbox.addEventListener('change', function(){
+        if(checkbox.checked){
+            passwordInput.type = "password";
+            
+            console.log("checked")
+        }
+        else{
+            passwordInput.type = "text";
+            console.log("unchecked")
+        }
+    })
+}
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Script loaded!');
     handleFloatingLabel();
