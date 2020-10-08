954cb74efb602595ff016b7ca651f1c48f9db736
CoudronLukas <lukas.coudron@student.howest.be>
2020-10-08 11:25:17 +0200

    Dark theme

--- script/app.js
+++ script/app.js
@@ -1,9 +1,15 @@
 function handleFloatingLabel() {
-   
+
 }
 
 function handlePasswordSwitcher() {
-    
+    var password = document.getElementById('ShowPassword');
+    if(password.type  === "password"){
+        password.type = "text";
+    }
+    else{
+        password.type = "password";
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
